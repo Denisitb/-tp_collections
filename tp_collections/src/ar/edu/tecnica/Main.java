e665cdd2ee8a29c8cb2e39f961246fd51e643411
Alumno <Alumno@host.docker.internal>
2024-11-08 15:42:34 -0300

    Tp-Collections

--- tp_collections/src/ar/edu/tecnica/Main.java
+++ tp_collections/src/ar/edu/tecnica/Main.java
@@ -1,7 +1,7 @@
 package ar.edu.tecnica;
 
 /**
- * @author NOMBRE Y APELLIDO ALUMNO
+ * @author DENIS ITURBE
  * 
  */
 
@@ -33,82 +33,327 @@ public class Main {
 		ejemploMax();
 	}
 
-	public static void ejemploList() {
-		// ArrayList
-		ArrayList<Integer> numeros = new ArrayList<Integer>();
-		// numeros.add(0);
+	
+	{ public static void main(String[] args) {
+	        
+	        ArrayList<Integer> numeros = new ArrayList<Integer>();
+
+
+	        numeros.add(1);
+	        numeros.add(2);
+	        numeros.add(3);
+	        numeros.add(4);
+	        numeros.add(5);
+	      
+	        System.out.println("1, 2, 3, 4, 5");
+	        for (Integer numeros : lista) {
+	            System.out.println(numero);
+	        }
+	    }
 	}
 
-	public static void ejemploSet() {
-		// HashSet
-		HashSet<Integer> numeros = new HashSet<Integer>();
-		// numeros.add(0);
+	
+	{ public static void main(String[] args) {
+	       
+	        HashSet<Integer> numeros = new HashSet<Integer>();
+
+	        numeros.add(1);
+	        numeros.add(2);
+	        numeros.add(3);
+	        nuemros.add(4);
+	        numeros.add(5);
+	        
+	        System.out.println("1, 2, 3, 4, 5");
+	        for (Integer numeros : set) {
+	            System.out.println(numeros);
+	        }
+	    }
+	
+	}
+	
+	
+	
+
+	{
+	    public static void main(String[] args) {
+
+	    	PriorityQueue<Integer> numeros = new PriorityQueue<Integer>();
+
+	        
+	        nuemros.add(5);
+	        nuemros.add(1);
+	        nuemros.add(3);
+	        numeros.add(4);
+	        numeros.add(2);
+
+	        
+	        System.out.println("5, 1, 3, 4, 2");
+	        while (!colaPrioridad.isEmpty()) {
+	           
+	            System.out.println(filaPrioridad.poll());
+	        }
+	    }
 	}
 
-	public static void ejemploQueue() {
-		// PriorityQueue
-		PriorityQueue<Integer> numeros = new PriorityQueue<Integer>();
-		// numeros.add(0);
+
+
+	{
+	    public static void main(String[] args) {
+	        
+	        ArrayDeque<Integer> numeros = new ArrayDeque<Integer>();
+
+	        
+	        numeros.add(1);
+	        numeros.add(2);
+	        numeros.add(3);
+	        numeros.add(4);
+	        numeros.add(5);
+
+	       
+	        System.out.println("2, 3, 4, 5");
+	        for (Integer numeros : deque) {
+	            System.out.println(numeros);
+	        }
+	    }
 	}
 
-	public static void ejemploDeque() {
-		// ArrayDeque
-		ArrayDeque<Integer> numeros = new ArrayDeque<Integer>();
-		// numeros.add(0);
-	}
-
+	
+	
+	
 	public static void ejemploMap() {
 		// HashMap
 		HashMap<String, Integer> numeros = new HashMap<String, Integer>();
 		// numeros.put("X", 0);
 	}
 
-	public static void loadSortedSet() {
-		// TreeSet
-		TreeSet<Integer> numeros = new TreeSet<Integer>();
-		// numeros.add(0);
+{
+	    public static void main(String[] args) {
+	        
+	    	HashMap<String, Integer> numeros = new HashMap<String, Integer>();
+
+	        
+	        numeros.put("Rojo", 1);
+	        numeros.put("Azul", 2);
+	        numeros.put("Verde", 3);
+	        numeros.put("Amarillo", 4);
+	        numeros.put("Rosa", 5);
+
+	        
+	        System.out.println("Rojo, Azul, Verde, Amarillo, Rosa");
+	        for (String clave : mapa.keySet()) {
+	            Integer numeros = mapa.get(clave);
+	            System.out.println(clave + " -> " + valor);
+	        }
+	    }
 	}
 
-	public static void loadSortedMap() {
-		// TreeMap
-		TreeMap<String, Integer> numeros = new TreeMap<String, Integer>();
-		// numeros.put("X", 0);
+	
+	
+{
+	    public static void main(String[] args) {
+
+	    	ArrayList<Integer> numeros = new ArrayList<Integer>();
+
+	        
+	        lista.add(5);
+	        lista.add(2);
+	        lista.add(4);
+	        lista.add(1);
+	        lista.add(3);
+
+	        System.out.println("5, 2, 4, 1, 3");
+	        for (Integer numeros : lista) {
+	            System.out.println(numero);
+	        }
+
+	        Collections.sort(lista);
+
+	        System.out.println("1, 2, 3, 4, 5");
+	        for (Integer numeros : lista) {
+	            System.out.println(numero);
+	        }
+	    }
 	}
 
-	public static void ejemploSort() {
-		// Collections.sort(null);
+
+
+
+	    {public static void main(String[] args) {
+	        
+	        ArrayList<Integer> nuemros = new ArrayList<Integer>();
+
+	     
+	        lista.add(1);
+	        lista.add(2);
+	        lista.add(3);
+	        lista.add(4);
+	        lista.add(5);
+
+	        
+	        System.out.println("1, 2, 3, 4, 5");
+	        for (Integer numeros : lista) {
+	            System.out.println(numero);
+	        }
+
+	        Collections.reverse(lista);
+
+
+	        System.out.println("5, 4, 3, 2, 1");
+	        for (Integer numeros : lista) {
+	            System.out.println(numero);
+	        }
+	    }
 	}
 
-	public static void ejemploReverse() {
-		// Collections.reverse();
+
+	{
+	    public static void main(String[] args) {
+	        
+	        ArrayList<Integer> numeros = new ArrayList<>();
+
+	        
+	        numeros.add(1);
+	        numeros.add(2);
+	        numeros.add(3);
+	        numeros.add(4);
+	        numeros.add(5);
+
+	  
+	        System.out.println("1, 2, 3, 4, 5");
+	        for (Integer numero : lista) {
+	            System.out.println(numero);
+	        }
+
+	    
+	        Collections.shuffle(lista);
+
+	        
+	        System.out.println("3, 5, 1, 2, 4");
+	        for (Integer numeros : lista) {
+	            System.out.println(numero);
+	        }
+	    }
 	}
 
-	public static void ejemploShuffle() {
-		// Collections.shuffle();
+	
+{
+	    public static void main(String[] args) {
+	      
+	        ArrayList<Integer> numeros = new ArrayList<Integer>();
+
+	     
+	        numeros.add(1);
+	        numeros.add(2);
+	        numeros.add(3);
+	        numeros.add(4);
+	        numeros.add(5);
+
+	        
+	        System.out.println("1, 2, 3, 4, 5");
+	        for (Integer numeros : lista) {
+	            System.out.println(numero);
+	        }
+
+	      
+	        Collections.swap(lista, 1, 3);
+
+	        // Imprimir la lista después del intercambio
+	        System.out.println("2, 4");
+	        for (Integer numero : lista) {
+	            System.out.println(numero);
+	        }
+	    }
 	}
 
-	public static void ejemploSwap() {
-		// Collections.swap();
+	
+
+	{
+	    public static void main(String[] args) {
+	        
+	        ArrayList<Integer> numeros = new ArrayList<Integer>();
+
+	        
+	        lista.add(1);
+	        lista.add(2);
+	        lista.add(3);
+	        lista.add(4);
+	        lista.add(5);
+
+	       
+	        System.out.println("1, 2, 3, 4, 5");
+	        for (Integer numeros : lista) {
+	            System.out.println(numero);
+	        }
+
+	        
+	        Collections.fill(lista, 10);
+
+	       
+	        System.out.println("10, 10, 10, 10, 10");
+	        for (Integer numeros : lista) {
+	            System.out.println(numero);
+	        }
+	    }
 	}
 
-	public static void ejemploFill() {
-		// Collections.fill();
+	
+
+	{
+	    public static void ejemploMin() {
+	       
+	        ArrayList<Integer> numeros = new ArrayList<Integer>();
+
+	        
+	        numeros.add(2);
+	        numeros.add(33);
+	        numeros.add(-32);
+
+	        
+	        var min = Collections.min(numeros);
+
+	        
+	        System.out.println("""
+	                El mínimo es:
+	                              """ + min);
+	    }
+
+	    public static void main(String[] args) {
+	        
+	        ejemploMin();
+	    }
 	}
 
-	public static void ejemploMin() {
-		ArrayList<Integer> numeros = new ArrayList<Integer>();
-		numeros.add(2);
-		numeros.add(33);
-		numeros.add(-32);
-		var min = Collections.min(numeros);
-
-		System.out.println("""
-				El mínimo es:
-				              """ + min);
-	}
-
+	
 	public static void ejemploMax() {
 		
 	}
 
+{
+	    public static void ejemploMax() {
+
+	    	ArrayList<Integer> numeros = new ArrayList<>();
+
+	       
+	        numeros.add(2);
+	        numeros.add(33);
+	        numeros.add(-32);
+	        numeros.add(50);
+	        numeros.add(15);
+
+	       
+	        var max = Collections.max(numeros);
+
+	  
+	        System.out.println("""
+	                El máximo es:
+	                              """ + max);
+	    }
+
+	    public static void main(String[] args) {
+	      
+	        ejemploMax();
+	    }
+	}
+
+	
 }
